203482171041e735c260cdcce55b7b5b3905b67a
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-10 10:34:26 +0000

    For...of Loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayF = [];
-  for(const idx of array) {
-    arrayF.push('${appendString}{idx}');
+  for (const idx of array) {
+    arrayF.push(`${appendString}${idx}`);
   }
 
   return arrayF;
