f81b61a89c939ece0e31e0ff2e3a2586efc9dc6d
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-10 21:09:37 +0000

    fixed lint issues

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,9 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from "./0-classroom"; // Removed ".js" extension
 
 export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34), // Added comma
   ];
 }
-
