1ba0fa7eb017aaf876fb6dd7396d2d695eacefc7
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-10 19:33:28 +0000

    throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,15 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+  
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    const errorMessage = error.toString();
+    queue.push(errorMessage);
   } finally {
     queue.push('Guardrail was processed');
   }
+  
   return queue;
 }
-
