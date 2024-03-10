bba53b5c4972a45fc64bffe4a53b784c9f92e5c6
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-10 19:36:01 +0000

    lint issues fixed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,15 +1,15 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-  
+
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    const errorMessage = error.toString();
+    const errorMessage = error.toString(); // Convert error to string
     queue.push(errorMessage);
   } finally {
     queue.push('Guardrail was processed');
   }
-  
+
   return queue;
 }
