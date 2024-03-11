bb0fe76d79385bb847a1ca0250a841b56c0fc20d
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-03-11 18:18:30 +0000

    fixed linting issues

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
+    // Enforce type validation constraints using `instanceof` for clarity
+    if (!(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
-    if (typeof length !== 'number') {
+    if (!(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+    if (!Array.isArray(students) || students.some((student) => !(student instanceof String))) {
       throw new TypeError('Students must be an array of strings');
     }
 
@@ -20,7 +21,7 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof name !== 'string') {
+    if (!(name instanceof String)) {
       throw new TypeError('Name must be a string');
     }
     this._name = name;
@@ -31,7 +32,7 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    if (typeof length !== 'number') {
+    if (!(length instanceof Number)) {
       throw new TypeError('Length must be a number');
     }
     this._length = length;
@@ -42,7 +43,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+    if (!Array.isArray(students) || students.some((student) => !(student instanceof String))) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
