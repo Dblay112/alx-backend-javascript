985050fc6883d9b901f87dc89aef499cf9e28820
Bright Selorm Djogatse <brightdjogatse@gmail.com>
2024-04-30 12:58:05 +0000

    Create a more complex HTTP server using Express

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,57 +1,11 @@
-const http = require('http');
-const fs = require('fs');
-const { argv } = require('process');
+const express = require('express');
 
-function countStudents(path, stream) {
-  if (fs.existsSync(path)) {
-    const data = fs.readFileSync(path, 'utf8');
-    const result = [];
-    data.split('\n').forEach((data) => {
-      result.push(data.split(','));
-    });
-    result.shift();
-    const newData = [];
-    result.forEach((data) => newData.push([data[0], data[3]]));
-    const fields = new Set();
-    newData.forEach((item) => fields.add(item[1]));
-    const final = {};
-    fields.forEach((data) => { (final[data] = 0); });
-    newData.forEach((data) => { (final[data[1]] += 1); });
-    stream.write(`Number of students: ${result.length}\n`);
-    const temp = [];
-    Object.keys(final).forEach((data) => temp.push(`Number of students in ${data}: ${final[data]}. List: ${newData.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}\n`));
-    // disable next-line
-    for (let i = 0; i < temp.length; i++) {
-      if (i === temp.length - 1) {
-        temp[i] = temp[i].replace(/(\r\n|\n|\r)/gm, '');
-      }
-      stream.write(temp[i]);
-    }
-  } else { throw new Error('Cannot load the database'); }
-}
+const app = express();
 
-const hostname = 'localhost';
-const port = 1245;
-
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  if (url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  }
-  if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      countStudents(argv[2], res);
-      res.end();
-    } catch (err) {
-      res.end(err.message);
-    }
-  }
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
 });
 
-app.listen(port, hostname);
+app.listen(1245);
 
 module.exports = app;
